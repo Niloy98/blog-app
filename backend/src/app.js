2631dc32a0cbd1bd9e22075150ea1275d5ebb980
Niloy Das <niloy06032002@gmail.com>
2025-08-20 23:41:42 +0530

    add path in server.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,10 @@
 import express from "express";
-import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import userRoute from "../src/routes/user.route.js"
 import blogRoute from "../src/routes/blog.route.js"
 import commentRoute from "../src/routes/comment.route.js"
 
-dotenv.config()
 const app = express();
 
 app.use(express.json());
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./database/index.js";
 
-dotenv.config()
+dotenv.config({
+    path: './.env'
+})
 
 const port = process.env.PORT || 8000
 
