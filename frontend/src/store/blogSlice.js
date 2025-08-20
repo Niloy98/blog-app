8eb528362280c3fe7604ddf1ae73479f4d3525d1
Niloy Das <niloy06032002@gmail.com>
2025-08-20 23:40:56 +0530

    blog app

--- frontend/src/store/blogSlice.js
+++ frontend/src/store/blogSlice.js
@@ -3,7 +3,7 @@ import {createSlice} from "@reduxjs/toolkit"
 const blogSlice = createSlice({
     name:"blog",
     initialState:{
-        blog:[],
+        blog:null,
         yourBlog:null
     },
     reducers:{
